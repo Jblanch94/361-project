052531a2bcdeaa934e0e9dc47769c6e54afac78b
Johnny Blanchard <johnnyblanchard94@gmail.com>
2020-02-13 10:25:16 -0500

    added a login page

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const path = require('path');
+const db = require('./models/db/db_config');
 
 const app = express();
+
+app.set('view engine', 'hbs');
+express.static(path.join(__dirname, './views'));
+
 app.use(express.urlencoded({extended: false}));
 app.use(session({
     secret: 'secret',
@@ -13,14 +18,15 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Test');
+    res.render('home.hbs');
 });
 
 app.get('/login', (req, res) => {
-
+    res.render('login.hbs');
 });
 
 app.post('/login', (req, res) => {
